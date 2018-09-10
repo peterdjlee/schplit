88ce1823a5fbf4e3beab2e76962799249df656f1
Peter Lee <peterdjlee@gmail.com>
2018-09-10 18:16:30 -0400

    re-write main and add colors

---
+++ lib/colors.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+const downBlue = const Color(0xFF2689FF);
+const downSalmon = const Color(0xFF17AEE8);
+
+const downErrorRed = const Color(0xFFC5032B);
+
+const downSurfaceWhite = const Color(0xFFFFFBFA);
+const downBackgroundWhite = Colors.white;
